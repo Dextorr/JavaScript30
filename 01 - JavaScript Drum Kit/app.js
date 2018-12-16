72ddd731e3f1d67045c1e5a0f2c1a5c18e050d5d
Dexter De Leon <dexdeleon@gmail.com>
2018-12-16 22:12:32 +0000

    DOM elements found in JS.

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
+  // Get DOM elements that will be used
+  const keyDivs = document.querySelectorAll('div.key')
+  const audioTags = document.querySelectorAll('audio')
+
+  keyDivs.forEach((div, i) => {
+    console.log(div, i)
+  })
 })
