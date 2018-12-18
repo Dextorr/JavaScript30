157dd278d509c9343c5bbf2ef91131b4fa245f6b
Dexter De Leon <dexdeleon@gmail.com>
2018-12-18 10:57:11 +0000

    Event listeners working with keyup playing appropriate audio tag.

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Get DOM elements that will be used
   const keyDivs = document.querySelectorAll('div.key')
-  const audioTags = document.querySelectorAll('audio')
 
-  keyDivs.forEach((div, i) => {
-    console.log(div, i)
+  function playSound(e){
+    // Get the audio tag that has the data-key property of the key that has been pressed
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+    // If the key doesn't have an audio tag with an appropriate data attribute, stop the function
+    if(!audio) return
+    // Reset the audio tag to time 0 and play it
+    audio.currentTime = 0
+    audio.play()
+  }
+
+  // Event Listener on document that passes the keyup event to the playSound function
+  document.addEventListener('keyup', (e) => {
+    playSound(e)
   })
 })
