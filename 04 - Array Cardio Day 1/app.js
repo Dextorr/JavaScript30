1fe538b287a23fd983e689128af39ed97e84ecc5
Dexter De Leon <dexdeleon@gmail.com>
2018-12-28 16:19:07 +0000

    First exercise complete.

--- 04 - Array Cardio Day 1/app.js
+++ 04 - Array Cardio Day 1/app.js
@@ -23,7 +23,12 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-
+const born1500s = inventors.filter((inventor) => {
+  if(inventor.year >= 1500 && inventor.year < 1600){
+    return inventor 
+  }
+})
+console.log(born1500s)
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
 
