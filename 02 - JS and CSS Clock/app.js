56656065d43eea06fdea56bf3318ef3a65a59e01
Dexter De Leon <dexdeleon@gmail.com>
2018-12-28 16:03:43 +0000

    Changed updateTime function to only add minutes and hours when the subsequent hand ticks over the 12 position on the clock.

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -8,12 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     min = now.getMinutes(),
     hr = now.getHours()
 
-  console.log(now)
+  // console.log(now)
 
   function updateTime(){
     sec += 1
-    min += 1/60
-    hr += 1/3600
+    if (sec%60 === 0) min += 1
+    if (min%60 === 0) hr += 1
     secHand.style.transform = `rotate(${90 + sec*6}deg)`
     minHand.style.transform = `rotate(${90 + min*6}deg)`
     hrHand.style.transform = `rotate(${90 + hr*30}deg)`
