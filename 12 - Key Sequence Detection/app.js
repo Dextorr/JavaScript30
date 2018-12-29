fcc929982670d1622a16181f8ac483e26f6eae68
Dexter De Leon <dexdeleon@gmail.com>
2018-12-29 22:15:40 +0000

    Key detection working with a simple alert once the Konami code is entered.

--- 12 - Key Sequence Detection/app.js
+++ 12 - Key Sequence Detection/app.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
+
+// Keycodes for konami code
+const konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13]
+// array to hold last 11 keys pressed
+const keysPressed = []
+
+// each time a keyup occurs...
+document.addEventListener('keyup', (e) => {
+  console.log(e.keyCode)
+  // ...that key's keycode is pushed to the keysPressed array
+  keysPressed.push(e.keyCode)
+  // this splice starts at 11 back from the end of the keysPressed array and removes everything before it
+  keysPressed.splice(-konamiCode.length - 1, keysPressed.length - konamiCode.length)
+  console.log(keysPressed)
+  // if the code is entered in correct sequence, then the alert is triggered
+  if (keysPressed.join('') === konamiCode.join('')) alert('Konami Code Recognised')
 })
