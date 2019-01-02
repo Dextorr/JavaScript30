8631a5b5085e478c7df31436cfeb43d2bcbfbf2f
Dexter De Leon <dexdeleon@gmail.com>
2019-01-02 17:20:59 +0000

    Fixed issue where hour hand would tick over each second that the minute hand points at 12.

--- 02 - JS and CSS Clock/app.js
+++ 02 - JS and CSS Clock/app.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateTime(){
     sec += 1
     if (sec%60 === 0) min += 1
-    if (min%60 === 0) hr += 1
+    if (min%60 === 0 && sec%60 === 0) hr += 1
     secHand.style.transform = `rotate(${90 + sec*6}deg)`
     minHand.style.transform = `rotate(${90 + min*6}deg)`
     hrHand.style.transform = `rotate(${90 + hr*30}deg)`
