1bdf035c0987d9e0ac0c23557d330084ea99a9c8
Dexter De Leon <dexdeleon@gmail.com>
2019-01-02 17:19:43 +0000

    Initial commit for Local Storage exercise.

---
+++ 15 - LocalStorage/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const addItems = document.querySelector('.add-items')
+  const itemsList = document.querySelector('.plates')
+  const items = []
+
+  console.log('working')
+})
